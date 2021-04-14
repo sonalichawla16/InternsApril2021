6bc1c3940b15a6825b09810651aa5289acefa689
Mayank Aggarwal <mayank.aggarwal@cybergindia.local>
2021-04-14 22:19:17 +0530

    Javascript Assignment by Mayank Aggarwal

---
+++ index.js
@@ -0,0 +1,12 @@
+function myfunction(){
+
+    window.open("https://www.linkedin.com/in/mayank-aggarwal-26770a11a/",'_blank');
+}
+function myfunction1(){
+
+    window.open("https://www.youtube.com/channel/UC4la5TtDULj0Nms5YhFg2jg",'_blank');
+}
+function myfunction2(){
+
+    window.open("https://www.facebook.com/mayank.aggarwal.75839",'_blank');
+}
