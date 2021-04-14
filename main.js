5b9b29a382ee8b8372fa0bf80b0b09b0c2762245
Divyanshu Goyal <divyanshu.goyal@cybergindia.local>
2021-04-14 21:46:18 +0530

    Updated Javascript Portfolio

---
+++ main.js
@@ -0,0 +1,18 @@
+function myfunction(){
+window.open("https://github.com/thealoneshadow");
+}
+function myfunction1(){
+    window.open("https://twitter/thealoneshadow");
+    }
+    function myfunction2(){
+        window.open("https://instagram/thealoneshadow");
+        }
+
+        function red() 
+{
+        var email = "divyanshu.goyal@cygrp.com"
+        var mailto_link = 'mailto:' + email
+        window = window.open(mailto_link, 'emailWindow')
+        if (window && window.open && !window.closed)         
+            window.close()
+}
