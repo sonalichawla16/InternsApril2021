ebd5ad8d79cd5838ac609a0ab31a94a694cc5637
Arpit Sharma <82054906+arpitsharma01@users.noreply.github.com>
2021-04-15 15:54:39 +0530

    script updated

---
+++ script.js
@@ -0,0 +1,12 @@
+function gmail(){
+    window.open("mailto: arpit1329.cse18@chitkara.edu.in");
+}
+
+function linkedin(){
+    window.open("https://in.linkedin.com/in/arpitsharma9568");
+
+}
+
+function github(){
+    window.open("https://github.com/arpitsharma17");
+}
