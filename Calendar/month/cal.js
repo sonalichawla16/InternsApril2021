5f1d6dc283c24cec75bf55d9fd60c7170abd7bb7
sakshamcygrp <82058333+sakshamcygrp@users.noreply.github.com>
2021-04-19 02:13:33 +0530

    Updated Calendar Commit By Saksham Grover

---
+++ Calendar/month/cal.js
@@ -0,0 +1,36 @@
+function jan(){
+    window.open("jan.html","_self");
+}
+function feb(){
+    window.open("feb.html","_self");
+}
+function mar(){
+    window.open("mar.html","_self");
+}
+function apr(){
+    window.open("apr.html","_self");
+}
+function may(){
+    window.open("may.html","_self");
+}
+function june(){
+    window.open("june.html","_self");
+}
+function july(){
+    window.open("july.html","_self");
+}
+function aug(){
+    window.open("aug.html","_self");
+}
+function sep(){
+    window.open("sep.html","_self");
+}
+function oct(){
+    window.open("oct.html","_self");
+}
+function nov(){
+    window.open("nov.html","_self");
+}
+function dec(){
+    window.open("dec.html","_self");
+}
