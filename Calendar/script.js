b39221848d34915f2443619266ab31e4b1e46d90
Arpit Sharma <82054906+arpitsharma01@users.noreply.github.com>
2021-04-19 13:46:57 +0530

    Calendar Uploaded

---
+++ Calendar/script.js
@@ -0,0 +1,57 @@
+function newYearAlert(){
+    alert("Holiday for both Dallas and Delhi Office🏦 [ Celebrates the arrival of New Year🍾🎊 ]");
+}
+function republicDayAlert(){
+    alert("Holiday for Delhi Office🏦 [ This day honours the date on which the constitution📕 of India came into effect ]");
+}
+function holiAlert(){
+    alert("Holiday for Delhi Office🏦 [ Holi celebrates the arrival of spring🍁, the end of winter🏂, the blossoming of love❤ ]");
+}
+function goodFridayAlert(){
+    alert("Holiday for both Dallas and Delhi Office🏦 [ Celebrates the Resurrection of Jesus✝ and his victory over death and sin and the celebration of Easter, the very pinnacle of Christian⛪ celebrations ]");
+}
+function ramzanAlert(){
+    alert("Holiday for Delhi Office🏦 [ Celebrated as the month during which Muhammad🕋 received the initial revelations of the Quran📗, the holy book for Muslims🕌. Fasting is one of the five fundamental principles of Islam ]");
+}
+
+function memorialDayAlert(){
+    alert("Holiday for Dallas Office🏦 [ Observed on the last Monday of May, honoring the men and women who died while serving in the U.S. military🎖 ]");
+}
+
+function independenceDayAlert(){
+    alert("Holiday for Dallas Office🏦 [ Independence Day 2021 in United States👮🏻‍♂️ ]");
+}
+
+function janmashtamiAlert(){
+    alert("Holiday for Delhi Office🏦 [ Hindu🕉 festival celebrating the birth of the god Krishna🛕 on the eighth day of the dark fortnight of the month of Bhadrapada ]");
+}
+
+function laborDayAlert(){
+    alert("Holiday for Dallas Office🏦 [ It constitutes a yearly national tribute to the contributions workers👷🏻‍♂️ have made to the strength💪🏻, prosperity, and well-being of our country⚒ ]");
+}
+
+function dussehraAlert(){
+    alert("Holiday for Delhi Office🏦 [ Dussehra🎇 festival is celebrated as the day when Lord Rama🚩 killed the demon king 🏹Ravana at Lanka i.e. victory of good🕉 over evil😈 ]")
+}
+function diwaliAlert()
+{
+    alert("Holiday for Delhi Office🏦 [ Celebrated🎇 as a significance of triumph🏹 of good over evil ]");
+}
+
+function thanksGivingAlert(){
+    alert("Holiday for Dallas Office🏦 [ Celebrating the harvest and other blessings👼🏻 of the past year📆 ]");
+}
+
+function eveAlert(){
+    alert("Holiday for Dallas Office🏦 [ Christmas Eve🍾 is the evening or entire day before Christmas Day✝, the festival commemorating the birth of Jesus🎊 ]");
+}
+
+function christmasAlert(){
+    alert("Holiday for both Dallas and Delhi Office🏦 [ Christmas is celebrated to remember the birth of Jesus Christ✝, who Christians believe is the Son of God⛪ ]");
+}
+
+function newEveAlert(){
+    alert("Holiday for Dallas Office🏦 [ It marks the last day of the year in the Gregorian calendar📅 and people celebrate🎊 New Year's Eve🍾 to bid farewell to the year that ends and to welcome the New Year📆.]");
+}
+
+
