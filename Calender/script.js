5b7b4e236a5be4f928420d101e8c3d8e6af1dce9
mansha-madaan <82077012+mansha-madaan@users.noreply.github.com>
2021-04-19 21:44:27 +0530

    Add files via upload

---
+++ Calender/script.js
@@ -0,0 +1,64 @@
+function loadfunc(){
+    const date=new Date();
+    document.querySelector(".date span").innerHTML=date.toDateString();
+}
+function holiday()
+{
+  document.getElementById("output").innerHTML = "<b>It's New Year in India and Dallas";
+}
+function holiday1()
+{
+  document.getElementById("output").innerHTML = "<b>It's Republic Day in India";
+}
+function holiday2()
+{
+  document.getElementById("output").innerHTML = "<b>It's Holi in India";
+}
+function holiday3()
+{
+  document.getElementById("output").innerHTML = "<b>Good Friday";
+}
+function holiday4()
+{
+  document.getElementById("output").innerHTML = "<b>India is celebrating Eid";
+}
+function holiday5()
+{
+  document.getElementById("output").innerHTML = "<b>Memorial Day in Dallas";
+}
+function holiday6()
+{
+  document.getElementById("output").innerHTML = "<b>Independence Day in Dallas";
+}
+function holiday7()
+{
+  document.getElementById("output").innerHTML = "<b>Janmashtami in India";
+}
+function holiday8()
+{
+  document.getElementById("output").innerHTML = "<b>Labor Day in Dallas";
+}
+function holiday9()
+{
+  document.getElementById("output").innerHTML = "<b>India is celebrating Diwali";
+}
+function holiday10()
+{
+  document.getElementById("output").innerHTML = "<b>Dallas is celebrating Thanksgiving";
+}
+function holiday11()
+{
+  document.getElementById("output").innerHTML = "<b>Christmas Eve in Dallas";
+}
+function holiday12()
+{
+  document.getElementById("output").innerHTML = "<b>India and Dallas are celebrating Christmas";
+}
+function holiday13()
+{
+  document.getElementById("output").innerHTML = "<b>New Year Eve in Dallas";
+}
+function clean()
+{
+  document.getElementById("output").innerHTML = "";
+}
